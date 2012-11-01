55b67836a3ed54938324ac2b40b50ad21bf801d1
Kane York <rikingcoding@gmail.com>
2012-11-01 07:46:16 -0700

    work on gamestarter

--- src/riking/stratgame/GameStarter.java
+++ src/riking/stratgame/GameStarter.java
@@ -4,19 +4,20 @@ public class GameStarter
 {
 	public void run()
 	{
-		World w = new World(9,9).setGlobal()
-				.addTeam(new Team(1,"RED"))
-				.addTeam(new Team(2,"BLU"))
-				.replaceTile(new TileSpawner(1), 1, 1)
-				.replaceTile(new TileSpawner(2), 8, 8)
-				;
+		World w = new World(9,9).setGlobal();
+		Team RED = new Team(1,"RED");
+		Team BLUE= new Team(2,"BLU");
+		TileSpawner redSpawn = new TileSpawner(RED, 1, 1);
+		TileSpawner bluSpawn = new TileSpawner(BLU, 8, 8);
+		w.addTeam(RED).addTeam(BLUE).replaceTile(redSpawn).replaceTile(bluSpawn);
+		redSpawn.scheduleFirst();
+		bluSpawn.scheduleFirst();
 		mainloop(w);
 	}
 	public void mainloop(World w)
 	{
-		while (true)
+		while (w.tick())
 		{
-			
 		}
 	}
 }
