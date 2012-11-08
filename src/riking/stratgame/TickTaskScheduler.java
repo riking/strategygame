61258151575e5f3a4f18ed8f7f983fe0f35ee5c7
Kane York <rikingcoding@gmail.com>
2012-11-08 13:05:56 -0800

    Update src/riking/stratgame/TickTaskScheduler.java

--- src/riking/stratgame/TickTaskScheduler.java
+++ src/riking/stratgame/TickTaskScheduler.java
@@ -18,19 +18,33 @@ public class TickTaskScheduler {
 	public void tick(long t)
 	{
 		currentTick = t;
-		System.out.println("tick "+Long.toString(t)+" (next "+Long.toString(tasks.first().tick));
-		Task temp = new TaskStub(t);
-		if(tasks.contains(temp))
-		{
-			Task toDo = tasks.ceiling(temp);
-			toDo.run();
-			tasks.remove(toDo);
-		}
-		if(tasks.isEmpty())
+		Task next = tasks.first()
+		if(next == null)
 		{
 			new Error("No more tasks to run!").printStackTrace();
 			World.getWorld().theGameMustGoOn = false;
+			return;
 		}
+		System.out.println("tick "+Long.toString(t)+" (next "+Long.toString(tasks.first().tick)+")");
+		if (next.tick < t)
+		{
+			System.err.println("WARNING: executing early tasks");
+			next = doPriorTasks(t); // Will return next task to run
+		}
+		if (next.tick > t) return;
+		next.run();
+		assert(tasks.remove(next));
+	}
+	private void doPriorTasks(long untilTick)
+	{
+		Task next = tasks.first();
+		while(next.tick < untilTick)
+		{
+			next.run();
+			assert(tasks.remove(next));
+			next = tasks.first();
+		}
+		return next; // return for chaining purposes
 	}
 	/**
 	 * Adds a task to run <i>delay</i> ticks later.
@@ -42,6 +56,34 @@ public class TickTaskScheduler {
 	{
 		newTask.tick = currentTick + delay;
 		add(newTask);
+	}
+	/**
+	 * remember to put the delay as the first argument!
+	 */
+	public void schedule(Class<? extends Task> taskClass, Object... args)
+	{
+		try {
+			args[0] += currentTick;
+		} catch (Throwable t)
+		{
+			throw new IllegalArgumentException("first argument of args must be a tick delay", t);
+		}
+		try {
+			Task newTask = taskClass.getConstructor(args).newInstance(args);
+			add(newTask);
+		}
+		catch (SecurityException impossible)
+		{ throw new RuntimeException(impossible); }
+		catch (IllegalAccessException impossible)
+		{ throw new RuntimeException(impossible); }
+		catch (InstantiationException e)
+		{ throw new RuntimeException("Attempt to construct an abstract Task"), e); }
+		catch (InvocationTargetException e)
+		{ throw new RuntimeException("An error occured when dynamically creating a Task"), e); }
+		catch (NoSuchMethodException, IllegalArgumentException e)
+		{
+			throw new IllegalArgumentEx
+		}
 	}
 	/**
 	 * Adds a task to the queue to run later. Assumes the tick is already set.
